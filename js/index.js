89e77ca8c4ee121fe6ea7e40e7b50971b540b20b
Ba-Zen <bazenatlas@gmail.com>
2019-04-08 20:32:06 -0400

    dom1

--- js/index.js
+++ js/index.js
@@ -40,3 +40,60 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let navA = document.querySelectorAll('a');
+let mainHeader = document.querySelector('h1');
+let headerButton = document.querySelector('button');
+let headerImg = document.getElementById('cta-img');
+let H4 = document.querySelectorAll('h4');
+let pG = document.querySelectorAll('p');
+let middleImg = document.getElementById('middle-img');
+
+
+//Doesn't work after adding items- returns nav is not defined 
+// let newA = document.createElement('a');
+// let newB = document.createElement('a');
+// newA.textContent = "One";
+// newB.textContent = "Two";
+// nav.appendChild(newA);
+// nav.appendChild(newB);
+// newA.style.color = "green";
+// newB.style.color = "green";
+
+
+navA[0].textContent = siteContent['nav']['nav-item-1'];
+navA[1].textContent = siteContent['nav']['nav-item-2'];
+navA[2].textContent = siteContent['nav']['nav-item-3'];
+navA[3].textContent = siteContent['nav']['nav-item-4'];
+navA[4].textContent = siteContent['nav']['nav-item-5'];
+navA[5].textContent = siteContent['nav']['nav-item-6'];
+
+
+navA.forEach(col=>col.style.color = "green");
+
+mainHeader.textContent = siteContent['cta']['h1'];
+
+headerButton.textContent = siteContent['cta']['button'];
+
+headerImg.setAttribute('src', siteContent['cta']['img-src']);
+
+H4[0].textContent = siteContent['main-content']['features-h4'];
+H4[1].textContent = siteContent['main-content']['about-h4'];
+H4[2].textContent = siteContent['main-content']['services-h4'];
+H4[3].textContent = siteContent['main-content']['product-h4'];
+H4[4].textContent = siteContent['main-content']['vision-h4'];
+middleImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+
+pG[0].textContent = siteContent['main-content']['features-content'];
+pG[1].textContent = siteContent['main-content']['about-content'];
+pG[2].textContent = siteContent['main-content']['services-content'];
+pG[3].textContent = siteContent['main-content']['product-content'];
+pG[4].textContent = siteContent['main-content']['vision-content'];
+
+H4[5].textContent = siteContent['contact']['contact-h4'];
+pG[5].textContent = siteContent['contact']['address'];
+pG[6].textContent = siteContent['contact']['phone'];
+pG[7].textContent = siteContent['contact']['email'];
+
+pG[8].textContent = siteContent['footer']['copyright'];
